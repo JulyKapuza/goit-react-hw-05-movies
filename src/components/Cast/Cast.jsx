93333867d096ad917574417b4d219ed3a9d5416f
Juliia Kapuza <kapuzajuliia@gmail.com>
2022-11-21 17:24:29 +0200

    update

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,13 +2,10 @@ import { fetchCast } from 'services/moviesApi';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import img from '../../default/coming.jpg';
-import {  useLocation } from 'react-router-dom';
 
 const Cast = () => {
   const { movieId } = useParams();
   const [cast, setCast] = useState(null);
-  const location = useLocation();
-  console.log(location);
 
   useEffect(() => {
     fetchCast(movieId).then(({ cast }) => {
@@ -22,28 +19,28 @@ const Cast = () => {
 
   return (
     <ul>
-      {cast.length > 0 ? cast.map(({ id, original_name, profile_path, character }) => {
-        return (
-          <li key={id}>
-            <img
-              src={
-                profile_path
-                  ? `https://image.tmdb.org/t/p/w200/${profile_path}`
-                  : img
-              }
-              alt={original_name}
-            />
-            <p>
-              <b>{original_name}</b>
-            </p>
-            <p>Character: {character}</p>
-          </li>
-        );
-      })
-      : (
+      {cast.length > 0 ? (
+        cast.map(({ id, original_name, profile_path, character }) => {
+          return (
+            <li key={id}>
+              <img
+                src={
+                  profile_path
+                    ? `https://image.tmdb.org/t/p/w200/${profile_path}`
+                    : img
+                }
+                alt={original_name}
+              />
+              <p>
+                <b>{original_name}</b>
+              </p>
+              <p>Character: {character}</p>
+            </li>
+          );
+        })
+      ) : (
         <p>We will add information soon</p>
-      )
-      }
+      )}
     </ul>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { App } from 'components/App';
-// import { BrowserRouter } from "react-router-dom";
-
-// import './index.css';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
-
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
